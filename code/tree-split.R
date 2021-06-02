2c97afd69a26298432de71325e386799ad8b88d9
danschw <danschw@iu.edu>
2021-06-01 23:34:49 -0400

    color tree by clade

--- code/tree-split.R
+++ code/tree-split.R
@@ -99,8 +99,8 @@ keep <- d.keep %>%
   pull(protein)
 
 ##### Save results #####
-if (!dir.exists(here("data", "reduced_set_faa"))){
-  dir.create(here("data", "reduced_set_faa"))
+if (!dir.exists(here("data", "reduced_set_to_align"))){
+  dir.create(here("data", "reduced_set_to_align"))
 }
 # filter multifasta of hsearch results for filtering
 library(seqinr)
@@ -115,17 +115,69 @@ keep_fa <- sigma_fa[c(which(header.protein %in% keep))]
 
 write.fasta(sequences = getSequence(keep_fa),
             names = getName(keep_fa),
-            file.out = here("data", "reduced_set_faa","sigmas_to_align.faa"))
+            file.out = here("data", "reduced_set_to_align","sigmas_to_align.faa"))
 
 # make filtered versions of meta data tables
 d.faa <- read_csv(here("data/sigmas_to_align.csv"))
 d.faa <- d.faa %>% 
   filter(protein %in% keep)
-write_csv(d.faa, here("data", "reduced_set_faa","sigmas_to_align.csv"))
+write_csv(d.faa, here("data", "reduced_set_to_align","sigmas_to_align.csv"))
 
 d.sp<- read_csv( here("data/bacterial_features.csv"))
 d.sp <- d.sp %>% 
   filter(product_accession %in% keep)
-write_csv(d.sp, here("data", "reduced_set_faa","bacterial_features.csv"))
+write_csv(d.sp, here("data", "reduced_set_to_align","bacterial_features.csv"))
 
 #################
+# function to mark phage only monophyletic clades
+# go over all internal nodes, divide to clade and check if one side is phage only
+# if yes mark as phage lade node
+
+internal <- d.iqt %>% 
+  filter(is.na(label)) %>% 
+  pull(node)
+
+#assign phage tip nodes to clade, and all other as empty vector
+d.iqt <- d.iqt %>% 
+  mutate(clade = if_else(group=="phage", "phage", "", missing = ""))
+
+for (i in internal){
+  x <- groupClade(iqt,.node=i)
+  
+  # add groups to main tree
+  d.x <- as_tibble(x) %>% 
+    select(node, cur.split = group) %>% 
+    left_join(d.iqt,., by = "node")
+  
+  # get groups as charcater vectors
+  g1 <- d.x %>% 
+    filter(cur.split == 1) %>% 
+    filter(! node %in% internal) %>% 
+    pull(group)
+  
+  g2 <- d.x %>% 
+    filter(cur.split == 0) %>% 
+    filter(! node %in% internal) %>% 
+    pull(group)
+  
+  # test if any of the groups is phage only
+  if (length(g1)==0) next # avoid empty charcter returning TRUE
+  if (length(g2)==0) next
+  if (all(g1=="phage") | all(g2=="phage")){
+    d.iqt$clade[d.iqt$node==i] <- "phage" # assign to phage only clade
+  }
+}
+
+
+
+as.treedata(d.iqt) %>% 
+  ggtree( layout = 'equal_angle', aes(color = clade))
+
+as.treedata(d.iqt) %>% 
+  ggtree( layout = 'radial', aes(color = clade))+
+  geom_tiplab(aes(label=tip.label), color="blue", size=3, offset = 1)
+
+as.treedata(d.iqt) %>% 
+  ggtree(aes(color = clade))+
+  geom_tippoint(aes(color=group), size=1, shape=20)+
+  geom_tiplab(aes(label=tip.label), color="blue", size=3, offset = .1)
